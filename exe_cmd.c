670b75244d1885735088079f8ba6e5927e4ba2a4
Pule Mathikha <122857549+TheeKingZa@users.noreply.github.com>
2023-04-25 18:30:58 +0200

    exe_cmd.c Execute command

---
+++ exe_cmd.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "cmdshell.h"
+
+/**
+* exe_cmd - Execute a command.
+*
+* @args: An array of arguments passed to the command.
+*
+* Return: 0 on success, 1 on failure.
+*/
+
+int exe_cmd(char *args)
+{
+/*Execute the command using the execvp() function */
+if (execvp(args[0], args) == -1)
+{
+/* If the execvp() function returns -1, there was an error */
+
+/* Print an error message with the name of the cmdshell program */
+perror("cmdshell");
+/* Return 1 to indicate failure */
+return (1);
+}
+/*Return 0 to indicate success */
+return (0);
+}
