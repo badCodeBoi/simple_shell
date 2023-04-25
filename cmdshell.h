a64e3c82171b0f89b66b7b1732dc7b67fee81bbe
Pule Mathikha <122857549+TheeKingZa@users.noreply.github.com>
2023-04-25 18:25:35 +0200

    cmdshell header file

---
+++ cmdshell.h
@@ -0,0 +1,37 @@
+#ifndef _CMDSHELL_H
+#define _CMDSHELL_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+extern char **environ;
+
+
+int main(void);
+void execute_command(void);
+void initialize_environment(void);
+
+
+void print_prompt(void);
+char *read_input(void);
+/*int execute_command(char *command);*/
+char **parse_line(char *line);
+int exe_cmd(char **args[]);
+
+/* exit_terminal*/
+void exit_terminal(void);
+void process_input(char *buffer);
+
+
+
+
+
+#endif /* _CMDSHELL_H */
