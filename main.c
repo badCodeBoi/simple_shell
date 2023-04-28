ad6c1ec1567c8c2b681ed528a2e628f71c1de341
Pule Mathikha <122857549+TheeKingZa@users.noreply.github.com>
2023-04-28 13:34:13 +0200

    main.c
    * Added sys/wait.h header file for waitpid() function.
    *Removed the @prompt and @cmdshell function parameters as they were not being used in the code.
    *Moved the removal of the trailing newline character from after scanf() to after fgets().
    *Replaced system() function with fork() and exec() functions to execute the command.
    *Added error handling for fork() and exec() functions.
    *Added waitpid() function to wait for the child process to finish.
    *Added error handling for waitpid() function.
    *Changed the exit status to be based on the exit status of the child process.

--- main.c
+++ main.c
@@ -1,26 +1,75 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h> /* added for waitpid() */
 
-/**
-* main - Cmdshell Enter "Entry Point"
-*
-* @prompt: prints > "#cisfun" to output.
-* @cmdshell: cmdshell is the commandline[Maxline]
-*/
+#define CMDSHELL_MAXLINE 100
 
 void prompt()
 {
 printf("#cisfun$ ");
 }
 
-int main()
+int main(void)
+{
+char cmdshell[CMDSHELL_MAXLINE];
+int cmd_stat;
+
+while (1)
 {
 prompt();
+if (fgets(cmdshell, CMDSHELL_MAXLINE, stdin) == NULL) {
+if (feof(stdin))
+{
+printf("\n");
+exit(EXIT_SUCCESS);
+}
+else
+{
+perror("fgets");
+exit(EXIT_FAILURE);
+}
+}
+/* remove trailing newline character */
 
-/* Wait for user input and print the command */
-char cmdshell[100];
+cmdshell[strcspn(cmdshell, "\n")] = '\0';
 
-scanf("%s", cmdshell);
-printf("You entered: %s\n", cmdshell);
+/* Execute the command */
+pid_t pid = fork(); // create a child process
+if (pid == -1)
+{
+perror("fork");
+exit(EXIT_FAILURE);
+}
+else if (pid == 0)
+
+{
+/* child process */
+execlp(cmdshell, cmdshell, NULL); // execute the command
+perror("execlp");
+exit(EXIT_FAILURE);
+}
+else
+{
+/* parent process */
+int status;
+waitpid(pid, &status, 0); // wait for child process to finish
+if (WIFEXITED(status)) { // check if child process exited normally
+cmd_stat = WEXITSTATUS(status); // get the exit status of child process
+}
+else
+{
+cmd_stat = -1;
+perror("waitpid");
+}
+}
+/* Check for errors */
+if (cmd_stat == -1) {
+perror("system");
+exit(EXIT_FAILURE);
+}
+}
 
 return (0);
 }
