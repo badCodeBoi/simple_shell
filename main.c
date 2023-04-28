5eedd89396440572078934587bdc4182bc154327
Pule Mathikha <122857549+TheeKingZa@users.noreply.github.com>
2023-04-28 14:02:29 +0200

    main.c
    eddited

--- main.c
+++ main.c
@@ -2,20 +2,30 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/wait.h>
+#include <sys/wait.h> /* added for waitpid() */
 
 #define CMDSHELL_MAXLINE 100
 
-void prompt()
+/**
+ * prompt - prints prompt string to stdout
+ *
+ * Return: void
+ */
+void prompt(void)
 {
 printf("#cisfun$ ");
 }
 
-int read_command(char* cmdshell)
+/**
+ * read_command - reads a command from stdin
+ *
+ * Return: pointer to command string
+ */
+char *read_command(void)
 {
+char *cmdshell = malloc(CMDSHELL_MAXLINE);
 if (fgets(cmdshell, CMDSHELL_MAXLINE, stdin) == NULL)
 {
-
 if (feof(stdin))
 {
 printf("\n");
@@ -26,18 +36,24 @@ else
 perror("fgets");
 exit(EXIT_FAILURE);
 }
-
 }
-
+/* remove trailing newline character */
 cmdshell[strcspn(cmdshell, "\n")] = '\0';
-return 0;
+
+return cmdshell;
 }
 
-int execute_command(char* cmdshell)
+/**
+ * execute_command - executes a command using fork() and execlp()
+ * @cmdshell: command to execute
+ *
+ * Return: void
+ */
+void execute_command(char *cmdshell)
 {
-pid_t pid = fork();
 int cmd_stat;
-
+/* Execute the command */
+pid_t pid = fork(); /* create a child process*/
 if (pid == -1)
 {
 perror("fork");
@@ -45,18 +61,21 @@ exit(EXIT_FAILURE);
 }
 else if (pid == 0)
 {
-execlp(cmdshell, cmdshell, NULL);
+/* child process */
+execlp(cmdshell, cmdshell, NULL); /* execute the command */
 perror("execlp");
 exit(EXIT_FAILURE);
 }
 else
 {
+/* parent process */
 int status;
-waitpid(pid, &status, 0);
+waitpid(pid, &status, 0); /* wait for child process to finish */
 
 if (WIFEXITED(status))
 {
-cmd_stat = WEXITSTATUS(status);
+/* check if child process exited normally */
+cmd_stat = WEXITSTATUS(status); /* get the exit status of child process */
 }
 else
 {
@@ -64,26 +83,28 @@ cmd_stat = -1;
 perror("waitpid");
 }
 }
-
+/* Check for errors */
 if (cmd_stat == -1)
 {
 perror("system");
 exit(EXIT_FAILURE);
 }
-
-return 0;
 }
 
+/**
+ * main - entry point
+ *
+ * Return: 0 on success
+ */
 int main(void)
 {
-char cmdshell[CMDSHELL_MAXLINE];
-
 while (1)
 {
 prompt();
-read_command(cmdshell);
+char *cmdshell = read_command();
 execute_command(cmdshell);
+free(cmdshell);
 }
 
-return 0;
+return (0);
 }
